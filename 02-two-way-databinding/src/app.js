cb16bafd65dafd7c39482eafa1beb0d5be8450b0
Robin Böhm <robin.boehm@symetics.com>
2015-09-04 13:59:33 +0200

    add 02-two-way-binding

---
+++ 02-two-way-databinding/src/app.js
@@ -0,0 +1,25 @@
+import 'zone.js/lib/browser/zone-microtask';
+import 'reflect-metadata';
+import 'babel-core/polyfill';
+import { assert } from 'rtts_assert/rtts_assert';
+
+import { bind} from 'angular2/di';
+import { Component, View, bootstrap} from 'angular2/angular2';
+import {FORM_DIRECTIVES} from 'angular2/forms';
+
+@Component({
+  selector : 'pizza-service'
+})
+@View({
+  directives: [FORM_DIRECTIVES],
+  template : `
+  <h1>Angular2 Tutorial von AngularJS.DE</h1>
+  <form>
+    <input type="text" [(ng-model)]="search">
+    <p>Du suchst gerade nach: {{search}}</p>
+  </form>
+  `
+}) class PizzaService {
+}
+
+bootstrap(PizzaService);
