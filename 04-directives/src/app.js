6a570f78444efdd4d1f36374a2cf4f79e482dcb1
Robin Böhm <robin.boehm@symetics.com>
2015-09-04 14:08:14 +0200

    add diff to complete 04-directives

--- 04-directives/src/app.js
+++ 04-directives/src/app.js
@@ -4,22 +4,20 @@ import 'babel-core/polyfill';
 import { assert } from 'rtts_assert/rtts_assert';
 
 import { bind} from 'angular2/di';
-import { Component, View, bootstrap} from 'angular2/angular2';
+import { Component, View, CORE_DIRECTIVES, bootstrap} from 'angular2/angular2';
 import {FORM_DIRECTIVES} from 'angular2/forms';
 
 @Component({
   selector : 'pizza-service'
 })
 @View({
-  directives: [FORM_DIRECTIVES],
+  directives: [FORM_DIRECTIVES,CORE_DIRECTIVES],
   template : `
   <h1>Angular2 Tutorial von AngularJS.DE</h1>
   <form>
     <input type="text" [(ng-model)]="search">
-    <p>Du suchst gerade nach: {{search.toUpperCase() + "!"}}</p>
+    <p *ng-if="search">Du suchst gerade nach: {{search}}</p>
   </form>
-
-  {{1 + 2 + 3}}
   `
 }) class PizzaService {
   constructor(){
